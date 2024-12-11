664fdc786d4ce629feb1c2ca7385fe73754cbeab
Naif Otibi <nn.n@outlook.cl>
2024-12-11 12:18:23 +0300

    adding the slider container

--- src/app/components/Password.tsx
+++ src/app/components/Password.tsx
@@ -6,11 +6,16 @@ export default function PasswordClient() {
   return (
     <div className="w-[50vw] p-4">
       <h1 className="text-xl text-center mb-4">Password Generator</h1>
-      <div className="flex justify-between border border-indigo-400 p-4">
+      <div className="flex justify-between border border-indigo-400 p-4 mb-4">
         <h2 className="text-xl font-bold text-indigo-300">password</h2>
-        <button>
+        <button aria-label="Copy Password">
             <Copy className="w-4 h-4 text-indigo-500" />
         </button>
+      </div>
+      <div className="border border-indigo-500 p-4">
+         <div className="flex gap-2">
+            
+         </div>
       </div>
     </div>
   );
