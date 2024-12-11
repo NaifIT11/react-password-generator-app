32aac66cc415c8846cc5aaeb87ad4678140ce228
Naif Otibi <nn.n@outlook.cl>
2024-12-11 12:12:06 +0300

    first appearence of password component

--- src/app/components/Password.tsx
+++ src/app/components/Password.tsx
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 
-
-
-export default function PasswordClient(){
-    
+export default function PasswordClient() {
+  return (
+    <div className="w-[50vw] p-4">
+      <h1 className="text-xl text-center mb-4">Password Generator</h1>
+      <div className="flex justify-between border border-indigo-400 p-4">
+        <h2 className="text-xl font-bold">password</h2>
+      </div>
+    </div>
+  );
 }
-
-
