7279370e8a25207eb16677c03f89fb424efd9592
Naif Otibi <nn.n@outlook.cl>
2024-12-11 12:14:40 +0300

    adding the copy button

--- src/app/components/Password.tsx
+++ src/app/components/Password.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { Copy } from "lucide-react";
+
 export default function PasswordClient() {
   return (
     <div className="w-[50vw] p-4">
       <h1 className="text-xl text-center mb-4">Password Generator</h1>
       <div className="flex justify-between border border-indigo-400 p-4">
-        <h2 className="text-xl font-bold">password</h2>
+        <h2 className="text-xl font-bold text-indigo-300">password</h2>
+        <button>
+            <Copy className="w-4 h-4 text-indigo-500" />
+        </button>
       </div>
     </div>
   );
